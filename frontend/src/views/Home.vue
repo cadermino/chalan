8b7f6aad54a991b65de4c7fcdab433defdf02455
carlos calderon <cadermino@gmail.com>
2020-01-22 15:06:52 -0600

    add landing

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,146 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!--Hero-->
+    <div
+      class="py-20"
+      style="background-image: url(../assets/shipping-boxes.jpg)"
+    >
+      <div class="container mx-auto px-6">
+        <h2 class="text-4xl font-bold mb-2 text-white">
+          Agenda tu vehiculo de mudanza en linea!
+        </h2>
+        <h3 class="text-2xl mb-8 text-gray-200">
+          Monitor your health vitals smartly anywhere you go.
+        </h3>
+        <button
+          class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider"
+        >
+          Pre Order
+        </button>
+      </div>
+    </div>
+    <!-- Features -->
+    <section class="container mx-auto px-6 p-10">
+      <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
+        Features
+      </h2>
+      <div class="flex items-center flex-wrap mb-20">
+        <div class="w-full md:w-1/2">
+          <h4 class="text-3xl text-gray-800 font-bold mb-3">
+            Exercise Metrics
+          </h4>
+          <p class="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch is able to capture you vitals
+            while you exercise. You can create different category of exercises
+            and can track your vitals on the go.
+          </p>
+        </div>
+        <div class="w-full md:w-1/2">
+          <img src="assets/health.svg" alt="Monitoring" />
+        </div>
+      </div>
+      <div class="flex items-center flex-wrap mb-20">
+        <div class="w-full md:w-1/2">
+          <img src="assets/report.svg" alt="Reporting" />
+        </div>
+        <div class="w-full md:w-1/2 pl-10">
+          <h4 class="text-3xl text-gray-800 font-bold mb-3">
+            Reporting
+          </h4>
+          <p class="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch can generate a comprehensive
+            report on your vitals depending on your settings either daily,
+            weekly, monthly, quarterly or yearly.
+          </p>
+        </div>
+      </div>
+      <div class="flex items-center flex-wrap mb-20">
+        <div class="w-full md:w-1/2">
+          <h4 class="text-3xl text-gray-800 font-bold mb-3">
+            Syncing
+          </h4>
+          <p class="text-gray-600 mb-8">
+            Our Smart Health Monitoring Wristwatch allows you to sync data
+            across all your mobile devices whether iOS, Android or Windows OS
+            and also to your laptop whether MacOS, GNU/LInux or Windows OS.
+          </p>
+        </div>
+        <div class="w-full md:w-1/2">
+          <img src="assets/sync.svg" alt="Syncing" />
+        </div>
+      </div>
+    </section>
+    <!-- Testimonials -->
+    <section class="bg-gray-100">
+      <div class="container mx-auto px-6 py-20">
+        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
+          Testimonials
+        </h2>
+        <div class="flex flex-wrap">
+          <div class="w-full md:w-1/3 px-2 mb-4">
+            <div class="bg-white rounded shadow py-2">
+              <p class="text-gray-800 text-base px-6 mb-5">
+                Monitoring and tracking my health vitals anywhere I go and on
+                any platform I use has never been easier.
+              </p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">
+                John Doe
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 px-2 mb-4">
+            <div class="bg-white rounded shadow py-2">
+              <p class="text-gray-800 text-base px-6 mb-5">
+                As an Athlete, this is the perfect product for me. I wear my
+                Smart Health Monitoring Wristwatch everywhere i go, even in the
+                bathroom since it's waterproof.
+              </p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">
+                Jane Doe
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 px-2 mb-4">
+            <div class="bg-white rounded shadow py-2">
+              <p class="text-gray-800 text-base px-6 mb-5">
+                I don't regret buying this wearble gadget. One of the best
+                gadgets I own!.
+              </p>
+              <p class="text-gray-500 text-xs md:text-sm px-6">
+                James Doe
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--Call to Action-->
+    <section style="background-color: #667eea">
+      <div class="container mx-auto px-6 text-center py-20">
+        <h2 class="mb-6 text-4xl font-bold text-center text-white">
+          Limited in Stock
+        </h2>
+        <h3 class="my-4 text-2xl text-white">
+          Get yourself the Smart Health Monitoring Wristwatch!
+        </h3>
+        <button
+          class="bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider"
+        >
+          Pre Order
+        </button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
