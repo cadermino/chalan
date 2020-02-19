3cd9f0af8cd23f54ad9673ebfa2fb70e044e11b5
carlos calderon <cadermino@gmail.com>
2020-02-19 17:59:54 -0600

    ssl lets encrypt

--- start-prod-ssl.sh
+++ start-prod-ssl.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(chalan.mx)
+domains=(chalan.mx www.chalan.mx)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cadermino@gmail.com" # Adding a valid address is strongly recommended
