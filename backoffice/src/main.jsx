4458fd6b5fe521f9454c6fad92894c7de25bd56d
Carlos <cadermino@gmail.com>
2026-04-13 17:46:52 -0500

    feat: configure backoffice for production (nginx routes + docker services)

--- backoffice/src/main.jsx
+++ backoffice/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/backoffice">
       <App />
       <Toaster position="top-right" />
     </BrowserRouter>
